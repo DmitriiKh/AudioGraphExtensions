101f7700286d07c43a41a07bbef35d6a2d8a91d2
Dmitry Khrustalev <dmitrii.khrustalev@gmail.com>
2020-09-02 13:07:33 -0300

    Add _progress and _status fields

--- AudioGraphExtensions/AudioGraphInputStream.cs
+++ AudioGraphExtensions/AudioGraphInputStream.cs
@@ -1,5 +1,6 @@
 ﻿namespace AudioGraphExtensions
 {
+    using System;
     using Windows.Media.Audio;
     using Windows.Storage;
     
@@ -7,14 +8,25 @@
     {
         private readonly IAudioInputNode _inputNode;
         private readonly AudioGraph _audioGraph;
+        private readonly Progress<double> _progress;
+        private readonly IProgress<string> _status;
 
-        protected AudioGraphInputStream(IAudioInputNode inputNode, AudioGraph audioGraph)
+        protected AudioGraphInputStream(
+            IAudioInputNode inputNode,
+            AudioGraph audioGraph,
+            Progress<double> progress,
+            IProgress<string> status)
         {
             _inputNode = inputNode;
             _audioGraph = audioGraph;
+            _progress = progress;
+            _status = status;
         }
 
-        public static AudioGraphInputStream FromFile(StorageFile file)
+        public static AudioGraphInputStream FromFile(
+            StorageFile file,
+            Progress<double> progress,
+            IProgress<string> status)
         {
             return null;
         }
--- AudioGraphExtensions/AudioGraphOutputStream.cs
+++ AudioGraphExtensions/AudioGraphOutputStream.cs
@@ -1,5 +1,6 @@
 ﻿namespace AudioGraphExtensions
 {
+    using System;
     using Windows.Media.Audio;
     using Windows.Storage;
 
@@ -7,14 +8,25 @@
     {
         private readonly IAudioNode _outputNode;
         private readonly AudioGraph _audioGraph;
+        private readonly Progress<double> _progress;
+        private readonly IProgress<string> _status;
 
-        protected AudioGraphOutputStream(IAudioNode outputNode, AudioGraph audioGraph)
+        protected AudioGraphOutputStream(
+            IAudioNode outputNode,
+            AudioGraph audioGraph,
+            Progress<double> progress,
+            IProgress<string> status)
         {
             _outputNode = outputNode;
             _audioGraph = audioGraph;
+            _progress = progress;
+            _status = status;
         }
 
-        public static AudioGraphOutputStream ToFile(StorageFile file)
+        public static AudioGraphOutputStream ToFile(
+            StorageFile file,
+            Progress<double> progress,
+            IProgress<string> status)
         {
             return null;
         }
