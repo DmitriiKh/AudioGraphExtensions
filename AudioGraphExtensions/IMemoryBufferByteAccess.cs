61a09547db4cbd6fbce701f114866c317395c4a5
Dmitry Khrustalev <dmitrii.khrustalev@gmail.com>
2020-09-02 12:56:28 -0300

    Add IMemoryBufferByteAccess interface

---
+++ AudioGraphExtensions/IMemoryBufferByteAccess.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AudioGraphExtensions
+{
+    public interface IMemoryBufferByteAccess
+    {
+        /// <summary>
+        ///     Declare COM interface
+        /// </summary>
+        [ComImport]
+        [Guid("5B0D3235-4DBA-4D44-865E-8F1D0E4FD04D")]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        internal unsafe interface IMemoryBufferByteAccess
+        {
+            void GetBuffer(out byte* buffer, out uint capacity);
+        }
+    }
+}
