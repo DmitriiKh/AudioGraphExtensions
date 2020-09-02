c0cfd7e291662a329d211a3b02a934338488cc49
Dmitry Khrustalev <dmitrii.khrustalev@gmail.com>
2020-09-02 17:31:07 -0300

    Transfer method

--- AudioGraphExtensions/AudioGraphOutputStream.cs
+++ AudioGraphExtensions/AudioGraphOutputStream.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Windows.Foundation;
+using Windows.Media;
 using Windows.Media.Audio;
 using Windows.Media.MediaProperties;
 using Windows.Media.Render;
+using Windows.Media.Transcoding;
 using Windows.Storage;
 
 namespace AudioGraphExtensions
@@ -11,28 +13,54 @@ namespace AudioGraphExtensions
     public class AudioGraphOutputStream
     {
         private readonly AudioGraph _audioGraph;
-        private readonly IAudioInputNode _inputNode;
         private readonly IAudioNode _outputNode;
-        private readonly Progress<double> _progress;
+        private readonly IProgress<double> _progress;
         private readonly IProgress<string> _status;
+        private int _audioCurrentPosition;
+        private TaskCompletionSource<bool> _writeFileSuccess;
+        private float[] _leftChannel;
+        private float[] _rightChannel;
+        private readonly Action _finalizer;
 
         protected AudioGraphOutputStream(
             IAudioInputNode inputNode,
             IAudioNode outputNode,
             AudioGraph audioGraph,
-            Progress<double> progress,
-            IProgress<string> status)
+            IProgress<double> progress,
+            IProgress<string> status,
+            Action finalizer)
         {
-            _inputNode = inputNode;
+            _finalizer = finalizer;
             _outputNode = outputNode;
             _audioGraph = audioGraph;
             _progress = progress;
             _status = status;
+            
+            // Connect nodes
+            inputNode.AddOutgoingConnection(outputNode);
         }
 
-        public static async Task<AudioGraphOutputStream> ToFile(
+        public async Task<bool> Transfer(float[] left, float[] rihgt = null)
+        {
+            _leftChannel = left;
+            _rightChannel = rihgt;
+            
+            // Reset position
+            _audioCurrentPosition = 0;
+
+            // Prepare to return results asynchronously
+            _writeFileSuccess = new TaskCompletionSource<bool>();
+
+            // Start process which will write audio samples frame by frame
+            // and will generated events QuantumStarted 
+            _audioGraph.Start();            
+
+            return await _writeFileSuccess.Task;
+        }
+
+        public async Task<AudioGraphOutputStream> ToFile(
             StorageFile file,
-            Progress<double> progress,
+            IProgress<double> progress,
             IProgress<string> status,
             uint sampleRate,
             uint channelCount)
@@ -52,12 +80,24 @@ namespace AudioGraphExtensions
                 resultNode.FileOutputNode,
                 resultGraph.Graph,
                 progress,
-                status);
+                status,
+                FileFinalizer);
 
             return stream;
         }
 
-        private static AudioFrameInputNode CreateAudioFrameInputNode(
+        private void FileFinalizer()
+        {
+            _audioGraph?.Stop();
+            _outputNode.Stop();
+            var result = ((AudioFileOutputNode) _outputNode)
+                .FinalizeAsync()
+                .GetResults();
+
+            _writeFileSuccess.TrySetResult(result == TranscodeFailureReason.None);
+        }
+
+        private AudioFrameInputNode CreateAudioFrameInputNode(
             uint sampleRate,
             uint channelCount,
             AudioGraph graph)
@@ -70,7 +110,91 @@ namespace AudioGraphExtensions
             var frameInputNode = graph.CreateFrameInputNode(
                 frameInputNodeProperties
             );
+            
+            frameInputNode.QuantumStarted += FrameInputNode_QuantumStarted;
+            
             return frameInputNode;
+        }
+        
+        private void FrameInputNode_QuantumStarted(
+            AudioFrameInputNode sender,
+            FrameInputNodeQuantumStartedEventArgs args)
+        {
+            var numSamplesNeeded = args.RequiredSamples;
+
+            if (numSamplesNeeded == 0 || _audioGraph is null)
+                return;
+
+            var (frame, finished) = ProcessOutputFrame(numSamplesNeeded);
+            
+            sender.AddFrame(frame);
+
+            if (finished)
+            {
+                _finalizer();
+
+                // clean status and progress 
+                _status.Report("");
+                _progress.Report(0);
+
+                return;
+            }
+
+            // to not report too many times
+            if (_audioGraph.CompletedQuantumCount % 100 == 0)
+            {
+                var dProgress =
+                    (double) 100 *
+                    _audioCurrentPosition /
+                    _leftChannel.Length;
+
+                _progress?.Report(dProgress);
+            }
+        }
+        
+        private unsafe (AudioFrame frame, bool finished)
+            ProcessOutputFrame(int requiredSamples)
+        {
+            var channelCount = _rightChannel is null ? 1u : 2u;
+
+            var bufferSize = (uint)(requiredSamples * sizeof(float) * channelCount);
+
+            var frame = new AudioFrame(bufferSize);
+
+            using (var buffer = frame.LockBuffer(AudioBufferAccessMode.Write))
+            using (var reference = buffer.CreateReference())
+            {
+                // Get the buffer from the AudioFrame
+                (reference as IMemoryBufferByteAccess).GetBuffer(
+                    out var dataInBytes,
+                    out var capacityInBytes);
+
+                // Cast to float since the data we are generating is float
+                var dataInFloat = (float*) dataInBytes;
+
+                var capacityInFloat = capacityInBytes / sizeof(float);
+
+                for (uint index = 0; index < capacityInFloat; index += channelCount)
+                {
+                    if (_audioCurrentPosition >= _leftChannel.Length)
+                    {
+                        // last frame can be not full
+                        return (frame, true);
+                    }
+
+                    dataInFloat[index] = _leftChannel[_audioCurrentPosition];
+
+                    // if output is stereo
+                    if (channelCount == 2 && _rightChannel != null)
+                    {
+                        dataInFloat[index + 1] = _rightChannel[_audioCurrentPosition];
+                    }
+
+                    _audioCurrentPosition++;
+                }
+            }
+
+            return (frame, false);
         }
 
         private static IAsyncOperation<CreateAudioGraphResult> CreateAudioGraphAsync()
--- AudioGraphExtensions/IMemoryBufferByteAccess.cs
+++ AudioGraphExtensions/IMemoryBufferByteAccess.cs
@@ -2,17 +2,14 @@
 
 namespace AudioGraphExtensions
 {
-    public interface IMemoryBufferByteAccess
+    /// <summary>
+    ///     Declare COM interface
+    /// </summary>
+    [ComImport]
+    [Guid("5B0D3235-4DBA-4D44-865E-8F1D0E4FD04D")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    internal unsafe interface IMemoryBufferByteAccess
     {
-        /// <summary>
-        ///     Declare COM interface
-        /// </summary>
-        [ComImport]
-        [Guid("5B0D3235-4DBA-4D44-865E-8F1D0E4FD04D")]
-        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-        internal unsafe interface IMemoryBufferByteAccess
-        {
-            void GetBuffer(out byte* buffer, out uint capacity);
-        }
+        void GetBuffer(out byte* buffer, out uint capacity);
     }
 }
