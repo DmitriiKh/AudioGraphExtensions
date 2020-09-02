3db7097f5228670bf111a5050a934955525df7a9
Dmitry Khrustalev <dmitrii.khrustalev@gmail.com>
2020-09-02 12:51:23 -0300

    Create AudioGraphInputStream

--- AudioGraphExtensions/AudioGraphInputStream.cs
+++ AudioGraphExtensions/AudioGraphInputStream.cs
@@ -1,7 +1,22 @@
 ﻿namespace AudioGraphExtensions
 {
+    using Windows.Media.Audio;
+    using Windows.Storage;
+    
     public class AudioGraphInputStream
     {
-        
+        private readonly IAudioInputNode _inputNode;
+        private readonly AudioGraph _audioGraph;
+
+        protected AudioGraphInputStream(IAudioInputNode inputNode, AudioGraph audioGraph)
+        {
+            _inputNode = inputNode;
+            _audioGraph = audioGraph;
+        }
+
+        public static AudioGraphInputStream FromFile(StorageFile file)
+        {
+            return null;
+        }
     }
 }
