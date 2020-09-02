f322a7530745195c06e308d8c5f52fd5dd8807fe
Dmitry Khrustalev <dmitrii.khrustalev@gmail.com>
2020-09-02 12:51:40 -0300

    Create AudioGraphOutputStream

--- AudioGraphExtensions/AudioGraphOutputStream.cs
+++ AudioGraphExtensions/AudioGraphOutputStream.cs
@@ -1,7 +1,22 @@
 ﻿namespace AudioGraphExtensions
 {
+    using Windows.Media.Audio;
+    using Windows.Storage;
+
     public class AudioGraphOutputStream
     {
-        
+        private readonly IAudioNode _outputNode;
+        private readonly AudioGraph _audioGraph;
+
+        protected AudioGraphOutputStream(IAudioNode outputNode, AudioGraph audioGraph)
+        {
+            _outputNode = outputNode;
+            _audioGraph = audioGraph;
+        }
+
+        public static AudioGraphOutputStream ToFile(StorageFile file)
+        {
+            return null;
+        }
     }
 }
